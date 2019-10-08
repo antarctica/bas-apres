0a8c1798929489d6191a3f538d34729ad2ff1373
Paul Breen <pbree@bas.ac.uk>
2019-10-08 15:25:35 +0100

    The conversion scripts can optionally take an explicit output filename

--- apres_to_nc.py
+++ apres_to_nc.py
@@ -5,12 +5,15 @@ from apres.apres import ApRESFile
 
 if __name__ == '__main__':
     try:
-        filename = sys.argv[1]
+        infile = sys.argv[1]
     except IndexError:
         progname = os.path.basename(sys.argv[0])
-        print('Usage: {} filename.dat'.format(progname))
+        print('Usage: {} infile.dat [outfile.nc]'.format(progname))
         sys.exit(2)
 
-    with ApRESFile(filename) as f:
-        f.to_netcdf()
+    # We can optionally be given an explicit output filename 
+    outfile = sys.argv[2] if len(sys.argv) > 2 else None
+ 
+    with ApRESFile(infile) as f:
+        f.to_netcdf(outfile)
 
